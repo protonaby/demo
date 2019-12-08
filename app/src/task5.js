3a7190e18dfba80f9d948f361b2656a98296996b
protonaby <protonaby@gmail.com>
2019-12-08 17:19:56 +0200

    implement luckyNumbers task with test

---
+++ app/src/task5.js
@@ -0,0 +1,27 @@
+export function countLuckyTickets(context) {
+  const min = context.min;
+  const max = context.max;
+  if (min.length != 6 || max.length != 6 || !/^[0-9]*$/.test(min) || !/^[0-9]*$/.test(max)) {
+    return {
+      status: 'failed',
+      reason: 'min and max should be strings representing numbers between 000000 and 999999'
+    };
+  }
+  let simple = 0;
+  let hard = 0;
+  for (let num = +min; num <= +max; num++) {
+    let s = num.toString().padStart(6, '0');
+    if (+s[0] + +s[1] + +s[2] == +s[3] + +s[4] + +s[5]) {
+      simple++;
+    } else if (+s[0] + +s[2] + +s[4] == +s[1] + +s[3] + +s[5]) {
+      hard++;
+    }
+  }
+  let winner = simple > hard ? 'Simple' : 'Hard';
+
+  return {
+    winner: winner,
+    simple: simple,
+    hard: hard
+  };
+}
---
+++ app/tests/test5.js
@@ -0,0 +1,27 @@
+import {
+  countLuckyTickets
+} from '../src/task5.js';
+
+function test5(assert) {
+  describe('countLuckyTickets function', function() {
+    it("countLuckyTickets({min: '000000', max:'111111'}) should return {winner: 'Simple', simple: 5134, hard: 4949}", function() {
+      assert.deepEqual(countLuckyTickets({min: '000000', max:'111111'}), {winner: 'Simple', simple: 5134, hard: 4949});
+    });
+    it("countLuckyTickets({min: '000000', max:'999999'}) should return {winner: 'Simple', simple: 55252, hard: 48552}", function() {
+      assert.deepEqual(countLuckyTickets({min: '000000', max:'999999'}), {winner: 'Simple', simple: 55252, hard: 48552});
+    });
+    it("countLuckyTickets({min: '000000', max:'-99999'}) should return error status", function() {
+      assert.deepEqual(countLuckyTickets({min: '000000', max:'-99999'}), {status: 'failed', reason: 'min and max should be strings representing numbers between 000000 and 999999'});
+    });
+    it("countLuckyTickets({min: '000000', max:'99999'}) should return error status", function() {
+      assert.deepEqual(countLuckyTickets({min: '000000', max:'99999'}), {status: 'failed', reason: 'min and max should be strings representing numbers between 000000 and 999999'});
+    });
+    it("countLuckyTickets({min: '000000', max:'99999'}) should return error status", function() {
+      assert.deepEqual(countLuckyTickets({min: '00000a', max:'999999'}), {status: 'failed', reason: 'min and max should be strings representing numbers between 000000 and 999999'});
+    });
+  });
+}
+
+export {
+  test5
+};
